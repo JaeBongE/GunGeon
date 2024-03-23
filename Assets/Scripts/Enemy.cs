5e7bb6f78c61981203afef147470ca5c7d2bf339
JaeBongE <chlwogus04@gmail.com>
2024-03-24 01:12:41 +0900

    03.23 동기화

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,11 +7,28 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] protected float maxHp;
     [SerializeField] protected float curHp;
-    Animator anim;
+    protected Animator anim;
+
+    [SerializeField] protected float maxX;
+    [SerializeField] protected float minX;
+    [SerializeField] protected float maxY;
+    [SerializeField] protected float minY;
+    [SerializeField] protected float moveSpeed;
+    protected Vector3 targetPos;
+    protected bool isMove = true;
+    protected float moveMaxCool = 2f;
+    protected float moveCool = 0f;
+
     private void Awake()
     {
         curHp = maxHp;
         anim = GetComponent<Animator>();
+        isMove = true;
+    }
+
+    private void Start()
+    {
+        targetPos = getRandomPos();
     }
 
     public virtual void GetDamage(float _damage)
@@ -20,13 +37,51 @@ public class Enemy : MonoBehaviour
         curHp -= _damage;
     }
 
-    private void Update()
+    public virtual void Update()
     {
-        if (curHp < 1)
+        moveRandom();
+        moveCoolTime();
+
+        //if (curHp < 1)
+        //{
+        //    gameObject.layer = LayerMask.NameToLayer("Nodamage");
+        //    anim.SetTrigger("Death");
+        //    Destroy(gameObject, 1f);
+        //}
+    }
+
+    public virtual void moveRandom()
+    {
+        if (isMove == false) return;
+
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPos, moveSpeed * Time.deltaTime);
+        
+        if (Vector3.Distance(gameObject.transform.position, targetPos) < 0.1f)
         {
-            gameObject.layer = LayerMask.NameToLayer("Nodamage");
-            anim.SetTrigger("Death");
-            Destroy(gameObject, 1f);
+            isMove = false;
+            targetPos = getRandomPos();
         }
+
+
+    }
+
+    private void moveCoolTime()
+    {
+        if (isMove == false)
+        {
+            moveMaxCool -= Time.deltaTime;
+            if (moveMaxCool < moveCool)
+            {
+                moveMaxCool = 2f;
+                isMove = true;
+            }
+        }
+    }
+
+    private Vector3 getRandomPos()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        return new Vector3(randomX, randomY, gameObject.transform.position.z);
     }
 }
--- Assets/Scripts/Soldier.cs
+++ Assets/Scripts/Soldier.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class Soldier : Enemy
 {
-    //protected override void Awake()
-    //{
-    //    base.Awake();
-    //}
 
-    //public override void GetDamage(float _damage)
-    //{
-    //    base.GetDamage(_damage);
+    public override void Update()
+    {
+        base.Update();
 
-    //    if (curHp <= maxHp * 0.5f)
-    //    {
-            
-    //    }
-    //}
+        if (curHp < 1)
+        {
+            gameObject.layer = LayerMask.NameToLayer("Nodamage");
+            anim.SetTrigger("Death");
+            Destroy(gameObject, 1f);
+        }
+    }
 
+    public override void moveRandom()
+    {
+        if (isMove == true)
+        {
+            anim.SetTrigger("Move");
+        }
+
+        Vector3 Pos = gameObject.transform.position;
+        Vector3 scale = gameObject.transform.localScale;
+        base.moveRandom();
+
+        if (Pos.x > targetPos.x)
+        {
+            scale.x = -1f;
+        }
+        else
+        {
+            scale.x = 1f;
+        }
+        gameObject.transform.localScale = scale;
+
+    }
+
+    public override void GetDamage(float _damage)
+    {
+        base.GetDamage(_damage);
+
+        anim.SetTrigger("Hit");
+    }
 }
