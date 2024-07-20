e66955b8ad3c7f27f9320f976f3ebca4eb54abf7
JaeBongE <chlwogus04@gmail.com>
2024-07-20 22:10:44 +0900

    07.20 빌드 1.00

--- Assets/Scripts/Staff.cs
+++ Assets/Scripts/Staff.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,7 @@ public class Staff : MonoBehaviour
 {
     Vector2 StaffPos;
     [SerializeField] float speed;
+    [SerializeField] GameObject obJect;
     [SerializeField] GameObject Fin;
     CanvasGroup canvas;
 
@@ -27,7 +29,8 @@ public class Staff : MonoBehaviour
 
         if(gameObject.transform.localPosition.x < -1961f)
         {
-            gameObject.transform.localPosition = new Vector2(-1961f, gameObject.transform.localPosition.y);
+            //gameObject.transform.localPosition = new Vector2(-1961f, gameObject.transform.localPosition.y);
+            obJect.SetActive(false);
             canvas.alpha += Time.deltaTime;
             if (canvas.alpha > 1f)
             {
